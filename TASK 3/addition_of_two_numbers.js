783fe3e58d0b51a90818e6e34642aed21dc2dabc
Selvakumaran <kumaranselva5444@gmail.com>
2025-09-22 10:34:14 +0530

    Add files via upload

---
+++ TASK 3/addition_of_two_numbers.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+  return a + b;
+}
+
+console.log("Traditional Function Result:", add(7, 17));
+
+const addArrow = (a, b) => a + b;
+console.log("Arrow Function Result:", addArrow(7, 17));
---
+++ TASK 3/map_function.js
@@ -0,0 +1,5 @@
+const fruits = ["apple", "banana", "orange", "mango"];
+
+const lengths = fruits.map(str => str.length);
+
+console.log("String Lengths:", lengths);
