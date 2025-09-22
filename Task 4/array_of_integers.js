9e34e5904c0c89374731393607deeb36b27f7d9f
Selvakumaran <kumaranselva5444@gmail.com>
2025-09-22 10:37:59 +0530

    Add files via upload

---
+++ Task 4/array_of_integers.js
@@ -0,0 +1,5 @@
+const numbers = [4, 7, 10, 12, 18, 25, 30];
+
+const greaterThan10 = numbers.filter(num => num > 10);
+
+console.log("Numbers greater than 10:", greaterThan10);
---
+++ Task 4/map_of_squares.js
@@ -0,0 +1,5 @@
+const nums = [1, 2, 3, 4, 5];
+
+const squares = nums.map(num => num * num);
+
+console.log("Squares:", squares);
